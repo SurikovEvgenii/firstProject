cd2334caae801b89b64f731e80cd3bf482d53ee2
Evgenii Surikov <surikov96@mail.ru>
2024-06-26 16:13:24 +0700

    Initial commit

---
+++ src/main/java/org/surikov/first_project/entities/data/ProfilePicture.java
@@ -0,0 +1,17 @@
+package org.surikov.first_project.entities.data;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.surikov.first_project.entities.accounts.DesignerAccount;
+
+@Data
+@Entity
+public class ProfilePicture {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String url;
+
+}
