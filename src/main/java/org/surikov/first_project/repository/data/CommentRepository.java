3f2861466a263bc46dab0dc361ad4336676bee3f
Evgenii Surikov <surikov96@mail.ru>
2024-07-02 19:46:00 +0700

    add findByProjectId

--- src/main/java/org/surikov/first_project/repository/data/CommentRepository.java
+++ src/main/java/org/surikov/first_project/repository/data/CommentRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.surikov.first_project.entities.data.Comment;
 
+import java.util.List;
+
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    public List<Comment> findByProjectId(Long id);
 }
